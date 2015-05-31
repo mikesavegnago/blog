77787aed5d5cd2efc46a854d7f46073738d04a7b
Paulo Cella <paulocella@unochapeco.edu.br>
2015-05-31 18:04:40 -0300

    adicionado logica do controller

--- module/Admin/src/Admin/Controller/LoginController.php
+++ module/Admin/src/Admin/Controller/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace Admin\Controller;
 
+use Zend\View\Model\ViewModel;
 use Zend\Mvc\Controller\AbstractActionController;
+use Core\Controller\ActionController as ActionController;
 
 /**
  * Controlador que para efetuar login
@@ -11,22 +13,27 @@ use Zend\Mvc\Controller\AbstractActionController;
  * @package Controller
  * @author Paulo Cella <paulocella@unochapeco.edu.br>
  */
-class LoginController extends AbstractActionController {
+class LoginController extends ActionController {
 
-    /**
+    
+
+     /**
      *
      * @return void
      */
-    public function loginAction() {
+    public function loginAction(){
         $em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
         $request = $this->getRequest();
         $session = $this->getServiceLocator()->get('Session');
-        if ($request->isPost()) {
+       
+        if ($request->isPost()){
             $values = $request->getPost();
+            var_dump($values);
             
-            try{
-                $this->getServiceLocator()->get('Admin\Service\Auth')->authenticate($values);
+            try{               
                 
+                $auth = $this->getServiceLocator()->get('\Admin\Service\Auth')->authenticate($values);
+                //var_dump($session->offsetGet('role'));exit;
                 if($session->offsetGet('role') == 'ADMIN'){
                      return $this->redirect()->toUrl('/admin/usuarios');
                 }else{
@@ -34,13 +41,12 @@ class LoginController extends AbstractActionController {
                 }    
             } catch (Exception $e) {
                 $this->flashMessenger()->addErrorMessage($e->getMessage());
-
             }
             
-            return $this->redirect()->toUrl('/');
+            return $this->redirect()->toUrl('/login/');
         }
         
-        return $this->request;
+         return new ViewModel();
     }
     
     public function logoutAction(){
